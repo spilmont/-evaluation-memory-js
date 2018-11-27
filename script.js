ea491672808a3744d5104de418ea7bf092773492
spilmont <43751712+spilmont@users.noreply.github.com>
2018-11-27 12:00:03 +0100

    a continuer

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 var Cartes = ["1up", "1up", "mushroom", "mushroom", "blueshell", "blueshell", "coin", "coin", "star", "star"];
-var MotifsCartes=[1,1,2,2,3,3,4,4,5,5];
-var EtatCartes = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-var DuoR=[];
-var bonnePaire=0;
+var valeurCarte=[1,1,2,2,3,3,4,4,5,5];
+var DuoR = [];
 var temp;
+var paireBonne=0;
+
+for (let i=0;i<Cartes.length;i++){
+    Cartes[i].value = valeurCarte[i];
+}
+
+DuoR=[];
+var face = 0;
+
+
+var afficheCartes = function () {
+    for (let i = 0; i < Cartes.length; i++) {
+        var cartes = document.createElement('img');
+        cartes.name = Cartes[i];
+        cartes.src="images/question.gif";
+        cartes.id = `carte${i}`;
+        cartes.style.width = `${150}px`;
+        cartes.style.height = `${150}px`;
+        cartes.style.marginLeft = `${8}%`;
+        cartes.style.marginTop = `${3}%`;
+        cartes.style.display="inline-block";
+        document.getElementById('plateau').appendChild(cartes);
+
+
+
+    }
+};
 
 
 var melange = function () {
@@ -15,17 +40,38 @@ var melange = function () {
         Cartes[i] = Cartes[alea];
         Cartes[alea] = temp;
     }
-};
+}
+
 
 melange();
+afficheCartes();
 
 for (let i = 0; i < Cartes.length; i++) {
-    document.getElementById('carte' + i).addEventListener("click", function () {
-        console.log(Cartes[i]);
-        document.getElementById('carte'+i).src = `images/${Cartes[i]}.png`;
 
 
-    })
+    document.getElementById("carte"+i).addEventListener("click", function () {
+
+        if(DuoR.length < 2) {
+            DuoR.push(Cartes[i]);
+            console.log(paireBonne);
+            console.log(DuoR);
+            document.getElementById("carte" + i).src = `images/${Cartes[i]}.png`;
+            if (DuoR.length ==2) {
+                DuoR[0].id = "choix"+i;
+
+                if (DuoR[0] === DuoR[1]) {
+                    paireBonne++;
+                    DuoR.splice(0, 2);
+                    alert("bonne paire");
+                } else {
+                    alert("mauvaise paire");
+                    document.getElementById("choix"+1).src="images/question.gif";
+                    document.getElementById("choix2"+2).src="images/question.gif";
+                    DuoR.splice(0,2);
+                }
+            }
+        }
+    });
 }
 
 
