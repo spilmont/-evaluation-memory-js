f6b7a26b4c35bb0b63eb68ed348ea8df8c96dc43
spilmont <43751712+spilmont@users.noreply.github.com>
2018-11-29 14:42:58 +0100

    memory js version 1.0

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var DuoR = [];
 var bonnePaire = 0;
 var seconde = 29;
 var seconde2 = 0;
-var temps = setTimeout(CaR, 1000);
+var temps=setTimeout(CaR,1000);
 var t = 0;
 var essais = 0;
 var carteid1;
@@ -17,7 +17,7 @@ var test;
 var afficheCartes = function () {
 
     //loop for repeat creation  many times as the length array:
-    for (let i = 0; i <Cartes.length; i++) {
+    for (let i = 0; i < Cartes.length; i++) {
         // creation of the image and this attributes:
         var cartes = document.createElement('img');
         // display the cards on the back face
@@ -34,7 +34,7 @@ var afficheCartes = function () {
 //random arrangement of the cards
 var melange = function () {
     //loop fo repeat operation on the all Cards
-    for (let i = 0; i <Cartes.length; i++) {
+    for (let i = 0; i < Cartes.length; i++) {
         var alea = Math.random() * Cartes.length;
         alea = Math.floor(alea);
         temp = Cartes[i];
@@ -49,75 +49,75 @@ afficheCartes();
 
 
 // loop for  click  of the all Card
-for (let i = 0; i <Cartes.length; i++) {
+for (let i = 0; i < Cartes.length; i++) {
 
     document.getElementById("carte" + i).addEventListener("click", function () {
 
 
-
         //if variable "t" less than 2 ....
-        if (t <2) {
+        if (t < 2) {
             // if variable 'test  is different from current index, condition executed
-            if ( i != test ){
+            if (i != test) {
                 // set test  to index for the upside condition
                 test = i;
-                console.log("execute : test = "+t);
-                console.log("tbl1 :"+tbl1);
+                console.log("execute : test = " + t);
+                console.log("tbl1 :" + tbl1);
 
-                    console.log("tbl1[1]: " +tbl1[1]);console.log("tbl1[0]: "+tbl1[0]);
-                    // ....so increase variable "essais" of 1
-                    essais++;
-                    // .... and so increase variable "t" of 1
-                    t++;
-                    //display a clicked card on the front face
-                    document.getElementById("carte" + i).src = `images/${Cartes[i]}.png`;
-                    // if t is strictly equal at 1......
-                    if (t === 1) {
-                        // ....so the index of  first card clicked is put a variable 'carteid1'
-                        carteid1 = i;
-                        console.log("id1" + carteid1);
+                console.log("tbl1[1]: " + tbl1[1]);
+                console.log("tbl1[0]: " + tbl1[0]);
+                // ....so increase variable "essais" of 1
+                essais++;
+                // .... and so increase variable "t" of 1
+                t++;
+                //display a clicked card on the front face
+                document.getElementById("carte" + i).src = `images/${Cartes[i]}.png`;
+                // if t is strictly equal at 1......
+                if (t === 1) {
+                    // ....so the index of  first card clicked is put a variable 'carteid1'
+                    carteid1 = i;
+                    console.log("id1" + carteid1);
 
-                    }
-                    // if t is strictly equal at 2......
-                    if (t === 2) {
-                        //....so the index second  card clicked is put a variable 'carteid2'
-                        carteid2 = i;
-                        console.log("id2" + carteid2);
+                }
+                // if t is strictly equal at 2......
+                if (t === 2) {
+                    //....so the index second  card clicked is put a variable 'carteid2'
+                    carteid2 = i;
+                    console.log("id2" + carteid2);
 
-                    }
+                }
 
 
-                    console.log(DuoR);
-                    // push the card  in the array 'Duor'
-                    DuoR.push(Cartes[i]);
-                    console.log(DuoR);
-                    // if t strictly equal 2 ......
-                    if (t === 2) {
-                        // compare if 'DuoR[0]' strictly equal 'DuoR[1]'
-                        if (DuoR[0] === DuoR[1]) {
-                            console.log("bonne pioche");
-                            // ...increase variable 'bonnePaire
-                            bonnePaire++;
-                            //... erase of the content of the array DuoR
-                            DuoR.splice(0, 2);
-                            // and variable 't' equal 0 for repeat condition
+                console.log(DuoR);
+                // push the card  in the array 'Duor'
+                DuoR.push(Cartes[i]);
+                console.log(DuoR);
+                // if t strictly equal 2 ......
+                if (t === 2) {
+                    // compare if 'DuoR[0]' strictly equal 'DuoR[1]'
+                    if (DuoR[0] === DuoR[1]) {
+                        console.log("bonne pioche");
+                        // ...increase variable 'bonnePaire
+                        bonnePaire++;
+                        //... erase of the content of the array DuoR
+                        DuoR.splice(0, 2);
+                        // and variable 't' equal 0 for repeat condition
+                        t = 0;
+                    } else {
+                        console.log("mauvaise poiche");
+                        // else if 'DuoR[0]' is different to 'DuoR[1]' create the timer for ...
+                        setTimeout(function () {
+                            //.... put variable 't' at 0
                             t = 0;
-                        } else {
-                            console.log("mauvaise poiche");
-                            // else if 'DuoR[0]' is different to 'DuoR[1]' create the timer for ...
-                            setTimeout(function () {
-                                //.... put variable 't' at 0
-                                t = 0;
-                                // and retourned the first ans second cards clicked
-                                document.getElementById("carte" + carteid1).src = "images/question.gif";
-                                document.getElementById("carte" + carteid2).src = "images/question.gif";
-                            }, 500);
-                            // erase tje content of the array DuoR for repeat conditions
-                            DuoR.splice(0, 2);
-
-                        }
+                            // and retourned the first ans second cards clicked
+                            document.getElementById("carte" + carteid1).src = "images/question.gif";
+                            document.getElementById("carte" + carteid2).src = "images/question.gif";
+                        }, 500);
+                        // erase tje content of the array DuoR for repeat conditions
+                        DuoR.splice(0, 2);
 
                     }
+
+                }
 
 
             }//condition block
@@ -132,7 +132,7 @@ for (let i = 0; i <Cartes.length; i++) {
             // ...display aside with id 'score'
             document.getElementById("score").style.display = "block";
             //... write the diw with id 'winorlose' the win message
-            document.getElementById("winorlose").innerHTML = `<h2>vous avez Gagnez!!!<br>vous avez reussi en ${seconde2}s <br>en ${essais} éssais</h2>`;
+            document.getElementById("winorlose").innerHTML = `<h2>vous avez Gagnez!!!<br>vous avez reussi en ${seconde2}s <br>en ${essais} essais</h2>`;
             // ...and hidden the countdown
             document.getElementById("temps").style.visibility = "hidden";
         } else {
@@ -144,8 +144,9 @@ for (let i = 0; i <Cartes.length; i++) {
     });
 
 }
+
 // function for create a countdown
-function CaR() {
+var CaR = function () {
     //write and decrease the seconds int the div with id 'time'
     document.getElementById("temps").innerHTML = "secondes restantes:" + seconde;
     // write a lose message in the div with id 'winorlose'
@@ -167,30 +168,32 @@ function CaR() {
         document.getElementById("temps").style.visibility = "hidden";
         // erase the timer of the function CaR
         clearTimeout(temps);
+
     }
 
 }
 
-// loop for repeat  returned card of the back face
-for (let i = 0; i < Cartes.length; i++) {
 
-    // if click on the button reset
-    document.getElementById('rejouer').addEventListener("click", function () {
-        /*location.reload();*/
+// if click on the button reset
+document.getElementById('rejouer').addEventListener("click", function () {
+    /*location.reload();*/
 
-        // initialized of the elements
-
-
-        bonnePaire = 0;
-        essais = 0;
-        seconde2 = 0;
-        seconde = 29;
+    // initialized of the elements
+    temps = setTimeout(CaR,1000);
+    bonnePaire = 0;
+    essais = 0;
+    seconde2 = 0;
+    seconde = 29;
+    // loop for repeat  returned card of the back face
+    for (let i = 0; i < Cartes.length; i++) {
         document.getElementById("carte" + i).src = `images/question.gif`;
-        document.getElementById('temps').style.visibility = "visible";
-        document.getElementById('score').style.display = "none";
-        document.getElementById('plateau').style.display = "block";
-        melange();
+    }
+    document.getElementById('temps').innerHTML ="secondes restantes: 30";
+    document.getElementById('temps').style.visibility = "visible";
+    document.getElementById('score').style.display = "none";
+    document.getElementById('plateau').style.display = "block";
+
+    melange();
 
 
-    });
-}
+});
--- test-unitaire.js
+++
@@ -1,9 +0,0 @@
-import {afficheCartes} from './script';
-
-describe('afficheCartes', function () {
-
-    it('should afficheCartes return true', function () {
-        expect(afficheCarte()).toBe(true);
-    });
-
-});
